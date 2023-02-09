abdebdcee3b4ffcffb8b11773b7d6350d34f2f54
SargentAli <95275905+SargentAli@users.noreply.github.com>
2023-02-10 01:03:32 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $currency = '₹';
-$db_username = 'root';
-$db_password = '';
+$db_username = 'userglenn';
+$db_password = 'thispasswordis@02';
 $db_name = 'bolt';
-$db_host = 'localhost';
+$db_host = 'gleserv.mysql.database.azure.com';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
